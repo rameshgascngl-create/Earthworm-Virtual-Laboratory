a8fc4b4b6c38916d06b5e637d2eea6119c5936bd
Ramesh R <rameshgascngl@gmail.com>
2026-09-26 10:38:04 +0530

    native v2: use View minimum-height API for accessible Home cards

--- app/src/main/java/in/ramesh/zoology/earthwormlab/NativeHomeActivity.java
+++ app/src/main/java/in/ramesh/zoology/earthwormlab/NativeHomeActivity.java
@@ -176,7 +176,7 @@ public final class NativeHomeActivity extends Activity {
         card.setBackground(bg);
         card.setClickable(true);
         card.setFocusable(true);
-        card.setMinHeight(dp(64));
+        card.setMinimumHeight(dp(64));
         card.setContentDescription(title+". "+detail);
         card.setMinimumHeight(dp(56));
 
